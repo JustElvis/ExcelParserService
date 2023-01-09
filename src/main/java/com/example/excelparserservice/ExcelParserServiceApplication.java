217766502929d4aef455d012d5835a63df0be63a
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2023-01-09 16:03:01 +0200

    second commit

---
+++ src/main/java/com/example/excelparserservice/ExcelParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelParserServiceApplication.class, args);
+	}
+
+}
