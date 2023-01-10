80dd1286d755df657397274baf6e66ac0c9eaa57
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2023-01-10 23:25:53 +0200

    add swagger

--- src/main/java/com/example/excelparserservice/controller/StudentController.java
+++ src/main/java/com/example/excelparserservice/controller/StudentController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,7 +34,6 @@ public class StudentController {
     @GetMapping("/save")
     public String save() {
         List<Student> students = excelParser.parse("students.xlsx");
-        students.forEach(System.out::println);
         studentService.saveAll(students);
         return "Done";
     }
--- src/main/java/com/example/excelparserservice/model/Student.java
+++ src/main/java/com/example/excelparserservice/model/Student.java
@@ -1,11 +1,10 @@
 package com.example.excelparserservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 
 @Entity
@@ -21,11 +20,12 @@ public class Student {
     private String email;
 
     public void assignStudent(Row row) {
+        DataFormatter formatter = new DataFormatter();
         id = (long) row.getCell(0).getNumericCellValue();
         name = row.getCell(1).getStringCellValue();
         lastName = row.getCell(2).getStringCellValue();
         age = (int) row.getCell(3).getNumericCellValue();
-        phoneNumber = row.getCell(4).getStringCellValue();
+        phoneNumber = formatter.formatCellValue(row.getCell(4));
         email = row.getCell(5).getStringCellValue();
     }
 }
